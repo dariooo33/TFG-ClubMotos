15cb1279dec77a48cdb260a81785f970dda710e4
Dariooo33 <darioruizm3@gmail.com>
2026-01-14 17:44:25 +0100

    Nuevo fondo, header mejorado

--- proyect/includes/header.php
+++ proyect/includes/header.php
@@ -1,13 +1,20 @@
-<header>
-    <ul>
-        <li><a href="">Tienda</a></li>
-        <li><a href="">Foro</a></li>
-        <li><a href="">Taller</a></li>
-        <div id="botonHome">
-            <li><a href="">Home</a></li>
-        </div>
-        <div id="botonPerfil">
-            <li><a href="">Perfil</a></li>               
-        </div>
-    </ul>
+<header class="header">
+<nav class="nav-left">
+    <a href="#">Tienda</a>
+    <a href="#">Foro</a>
+    <a href="#">Taller</a>
+</nav>
+
+<div class="nav-center">
+    <a href="#" class="home-btn">Home</a>
+</div>
+
+<nav class="nav-right">
+    <a href="#" class="profile-btn">
+    <svg width="22" height="22" viewBox="0 0 24 24" fill="none">
+        <circle cx="12" cy="8" r="4" stroke="currentColor" stroke-width="2"/>
+        <path d="M4 20c0-4 4-6 8-6s8 2 8 6" stroke="currentColor" stroke-width="2"/>
+    </svg>
+    </a>
+</nav>
 </header>
