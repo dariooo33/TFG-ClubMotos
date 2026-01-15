7f09d3b408d3aa34ee2d42c0e45be40ed5aec86c
userzzalee <agildelamo@gmail.com>
2026-01-15 19:43:28 +0100

    Fronted del login termiando

--- proyect/login.php
+++ proyect/login.php
@@ -1,19 +1,5 @@
 <?php
-    include_once "config/dbConect.php"; // Esto conecta y selecciona la base de datos
-    include_once "includes/functions.php"; //Añade funciones predefinidas en functions.php
-
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $errores = [];
-
-        $correo = $_POST['email'];
-        if(empty($correo)){
-            $errores[] = "El campo email esta vacio";
-        }elseif(!verificarEmail($correo)){
-            $errores[] = "Email invalido";
-        }else{
-            $consult = "SELECT contrasena FROM Usuario WHERE email = '$correo'";
-        }
-    }
+include_once "includes/header.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,31 +8,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="styles/login.css">
-    <link rel="stylesheet" href="styles/fondo.css">
     <link rel="stylesheet" href="styles/header.css">
-    <link rel="stylesheet" href="styles/footer.css">
-
 </head>
+
 <body>
-<?php
-    include_once "includes/header.php"
-?>
 <main>
-    <h1>INICIA SESION EN TU CUENTA</h1>
-    <hr>
-    <br>
-    <form method="post">
-        <input type="email" placeholder="Correo electronico"><br>
-        <input type="password" placeholder="Contraseña"><br>
-        <button type="button">Inicia sesion</button>
-    </form>
-    <br>
-    
-    <p><span>¿No tienes una cuetna?</span> <a href="">REGISTRATE</a></p>
-    <a href="">¿Olvidaste tu contraseña?</a>
-</main>
-<?php
-    include_once "includes/footer.php"
-?>    
+    <div class="recuadro">
+        <h2 class="titulo">INICIA SESION EN TU CUENTA</h2>
+        <hr><br>
+        <form method="post">
+            <!-- <button type="button" class="googleBoton"> INICIA SESION A TRAVES DE GOOGLE </button> -->
+            <input type="email" placeholder="Correo electronico"><br>
+            <input type="password" placeholder="Contraseña"><br>
+            <button type="button" class="iniciaSesion">Inicia sesion</button>
+        </form>
+        
+        <br>
+        <p><span>¿No tienes una cuenta?</span> <a href="" class="regi">REGISTRATE</a></p>
+        <br>
+        <a href="" class="olvido">¿Olvidaste tu contraseña?</a>
+    </div>
+</main>  
 </body>
-    </html>
+</html>
